170fee88b846d0e811170db412cbb277ea4f863d
satriam <satriabae43@gmail.com>
2023-09-18 21:56:37 +0700

    18092023-penambahan fitur hauling

---
+++ src/api/hauling/controllers/hauling.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hauling controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::hauling.hauling');
---
+++ src/api/hauling/routes/hauling.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hauling router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::hauling.hauling');
