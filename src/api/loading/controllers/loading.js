422b4d23ddcb4ccafc84e18dd95636e7b48de129
satriam <satriabae43@gmail.com>
2023-09-25 15:20:55 +0700

    25092023-loading sudah aman

--- src/api/loading/controllers/loading.js
+++ src/api/loading/controllers/loading.js
@@ -6,4 +6,32 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::loading.loading')
+module.exports = createCoreController('api::loading.loading', ({ strapi }) => ({
+    async find(ctx) {
+      // Calling the default core action
+      const { data, meta } = await super.find(ctx);
+      const query = strapi.db.query('api::loading.loading');
+      await Promise.all(
+        data.map(async (item, index) => {
+          const foundItem = await query.findOne({
+            where: {
+              id: item.id,
+            },
+            populate: ['createdBy', 'updatedBy'],
+          });
+  
+          data[index].attributes.createdBy = {
+            id: foundItem.createdBy.id,
+            firstname: foundItem.createdBy.firstname,
+            lastname: foundItem.createdBy.lastname,
+          };
+          data[index].attributes.updatedBy = {
+            id: foundItem.updatedBy.id,
+            firstname: foundItem.updatedBy.firstname,
+            lastname: foundItem.updatedBy.lastname,
+          };
+        })
+      );
+      return { data, meta };
+    },
+  }));
